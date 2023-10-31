364cae6ee5b11586b739d50f59eb540368b8aa9c
praveen-karre <karrepraveen.de@gmail.com>
2023-10-31 14:09:22 +0100

    TICKET01_Trading_Signal_Processing: Initial commit

---
+++ src/main/java/com/db/merchantsolutions/trading/TradingApplication.java
@@ -0,0 +1,14 @@
+package com.db.merchantsolutions.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantSolutionsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MerchantSolutionsApplication.class, args);
+    }
+
+}
